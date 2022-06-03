54a4d67779456c08bff838dfbafbe65369536b87
syifanaffh <syifanaffh@gmail.com>
2022-06-03 18:37:33 +0700

    UTSPraktik-IF6-10119248-SyifaNurAfifahh

---
+++ app/src/main/java/com/example/utspraktik_if6_10119248_syifanurafifahh/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.utspraktik_if6_10119248_syifanurafifahh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

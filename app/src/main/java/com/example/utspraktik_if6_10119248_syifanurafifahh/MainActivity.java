48d7cb91fbb15c19513ca2b31784d48c61de60ae
syifanaffh <87487965+syifanaffh@users.noreply.github.com>
2022-06-03 19:28:30 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/utspraktik_if6_10119248_syifanurafifahh/MainActivity.java
+++ app/src/main/java/com/example/utspraktik_if6_10119248_syifanurafifahh/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.utspraktik_if6_10119248_syifanurafifahh;
 
+// 26-04-2022, NIM:10119248, Syifa Nur Afifah, IF-6
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
